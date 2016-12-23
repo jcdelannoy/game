e7baa4f9216aec2559eba16a1a13bbda1575c360
JC <jcdelannoy@gmail.com>
2016-12-23 09:59:11 -0800

    OpenGL setup.

--- main.cpp
+++ main.cpp
@@ -1,109 +1,64 @@
-#include <windows.h>
-#include <GL/GL.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#pragma comment (lib, "opengl32.lib")
+// open GL
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+const GLint WIDTH = 640, HEIGHT = 480;
 
-int WinMain(__in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in_opt LPSTR lpCmdLine, __in int nShowCmd)
+int main()
 {
-    MSG msg = { 0 };
-    WNDCLASS wc = { 0 };
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = hInstance;
-    wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
-    wc.lpszClassName = "oglversionchecksample";
-    wc.style = CS_OWNDC;
-    if (!RegisterClass(&wc))
-        return 1;
-    //CreateWindowW(wc.lpszClassName, "openglversioncheck", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 640, 480, 0, 0, hInstance, 0);
+    // init GLFW - an open source, multi-platform library for OpenGL, OpenGL ES and Vulkan dev.
+    // It proves an API for creating windows, contexts and surfaces, receiving input and events.
+    if (!glfwInit())
+    {
+        fprintf(stderr, "GLFW failed to initialize.");
+        return EXIT_FAILURE;
+    }
 
-    while (GetMessage(&msg, NULL, 0, 0) > 0)
-        DispatchMessage(&msg);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);    
+    //glfwWindowHint(GLFW_SAMPLES, 4);
 
-    return 0;
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Game", nullptr, nullptr);
+
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    if (window == nullptr)
+    {
+        fprintf(stderr, "Failed to create GLFW window.");
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window);
+
+    // init GLEW - OpenGL Extension Wrangler.
+    // It provides efficient run-time mechanims for determining which OpenGL extensions are supported on the target platform.
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        fprintf(stderr, "Failed to initialize GLEW.");
+        return EXIT_FAILURE;
+    }
+
+    glViewport(0, 0, screenWidth, screenHeight);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw opengl stuff.
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_CREATE:
-    {
-        PIXELFORMATDESCRIPTOR pfd =
-        {
-            sizeof(PIXELFORMATDESCRIPTOR),
-            1,
-            PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
-            PFD_TYPE_RGBA,            //The kind of framebuffer. RGBA or palette.
-            32,                        //Colordepth of the framebuffer.
-            0, 0, 0, 0, 0, 0,
-            0,
-            0,
-            0,
-            0, 0, 0, 0,
-            24,                        //Number of bits for the depthbuffer
-            8,                        //Number of bits for the stencilbuffer
-            0,                        //Number of Aux buffers in the framebuffer.
-            PFD_MAIN_PLANE,
-            0,
-            0, 0, 0
-        };
-
-        HDC ourWindowHandleToDeviceContext = GetDC(hWnd);
-
-        int  letWindowsChooseThisPixelFormat;
-        letWindowsChooseThisPixelFormat = ChoosePixelFormat(ourWindowHandleToDeviceContext, &pfd);
-        SetPixelFormat(ourWindowHandleToDeviceContext, letWindowsChooseThisPixelFormat, &pfd);
-
-        HGLRC ourOpenGLRenderingContext = wglCreateContext(ourWindowHandleToDeviceContext);
-        wglMakeCurrent(ourWindowHandleToDeviceContext, ourOpenGLRenderingContext);
-
-        MessageBoxA(0, (char*)glGetString(GL_VERSION), "OPENGL VERSION", 0);
-
-        wglDeleteContext(ourOpenGLRenderingContext);
-        PostQuitMessage(0);
-    }
-    break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-
-}
-
-
-//#include <Windows.h>
-//#include <stdio.h>
-//#include <GL/GL.h>
-//
-//int main(void)
-//{
-//    printf("Hello Game.\n");
-//    return(0);
-//}
-//
-//void display(HDC deviceContext)
-//{
-//    const float windowWidth = 50.0f;
-//    const float windowHeight = 50.0f;
-//    glViewport(0, 0, windowWidth, windowHeight);
-//    glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
-//    glClear(GL_COLOR_BUFFER_BIT);
-//    SwapBuffers(deviceContext);
-//}
-//
-//void initOpenGL(HWND window)
-//{
-//    HDC windowDC = GetDC(window);
-//    HGLRC openGLRC = wglCreateContext(windowDC);
-//    if (wglMakeCurrent(windowDC, openGLRC))
-//    {
-//        // success.
-//    }
-//    else
-//    {
-//        // failure.
-//    }
-//    ReleaseDC(window, windowDC);
-//}
