7e942369033e2ae8255d53b442d25c30f7742e0c
JC <jcdelannoy@gmail.com>
2016-12-25 20:11:08 -0800

    Shaders.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cmath>
 
 // GLEW
 #define GLEW_STATIC
@@ -8,24 +9,33 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
+// misc.
+#include "shader.h"
+
 // audio headers
 #include <Windows.h>
 #include <mmsystem.h>
 
 const GLint WIDTH = 640, HEIGHT = 480;
 
-const GLchar* vertexShaderSource = "#version 330 core\n"
-"layout ( location = 0 ) in vec3 position;\n"
+const GLchar* vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"layout (location = 1) in vec3 color;\n"
+"out vec3 ourColor;\n"
 "void main( )\n"
 "{\n"
-"gl_Position = vec4( position.x, position.y, position.z, 1.0 );"
+"gl_Position = vec4( position.x, position.y, position.z, 1.0 );\n"
+"ourColor = color;\n"
 "}";
 
-const GLchar* fragmentShaderSource = "#version 330 core\n"
+const GLchar* fragmentShaderSource = 
+"#version 330 core\n"
+"in vec3 ourColor;\n"
 "out vec4 color;\n"
 "void main( )\n"
 "{\n"
-"color = vec4( 1.0f, 0.5f, 0.2f, 1.0f );\n"
+"color = vec4(ourColor, 1.0f);\n"
 "}";
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
@@ -68,6 +78,8 @@ int main()
     }
     glViewport(0, 0, screenWidth, screenHeight);
 
+    Shader ourShader("default.vs", "default.frag");
+
     // compile vertex shader - handles the processing of individual vertices.
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -94,7 +106,7 @@ int main()
         fprintf(stderr, infoLog);
     }
 
-    // link shaders.
+    // link shaders into shader program.
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
@@ -113,12 +125,13 @@ int main()
 
     GLfloat vertices[] =
     {
-        -0.5f, -0.5f, 0.0f, // bottom left
-        0.5f, -0.5f, 0.0f, // bottom right
-        0.0f, 0.5f, 0.0f // top middle
+        // positions            // colors
+        0.5f,   -0.5f,  0.0f,   1.0f,   0.0f,   0.0f, // bottom right
+        -0.5f,  -0.5f,  0.0f,   0.0f,   1.0f,   0.0f,// bottom left
+        0.0f,   0.5f,   0.0f,   0.0f,   0.0f,   1.0f // top left
     };
 
-    // generate Vertex Buffer Object and Vertex Array Object.
+    // generate Vertex Buffer Object and Vertex Array Object and Element Buffer Object.
     GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -128,15 +141,19 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid * ) 0);
-    glEnableVertexAttribArray(0);
+    // position attribute.
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0); 
+    glEnableVertexAttribArray(0); // location = 0
+
+    // color attribute.
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1); // location = 1
     
     // cleanup.
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
     // main loop.
-	PlaySound(TEXT("./audioFiles/testViolin.wav"), NULL, SND_FILENAME);
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
@@ -144,7 +161,10 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // activate the shader.
         glUseProgram(shaderProgram);
+
+        // draw the vertices.
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
