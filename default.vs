20b38225d640f21eea920c11d9030e2da4cc8852
JC <jcdelannoy@gmail.com>
2016-12-27 19:44:54 -0800

    3D testing.

--- default.vs
+++ default.vs
@@ -1,17 +1,16 @@
 #version 330 core
 layout (location = 0) in vec3 position;
-layout (location = 1) in vec3 color;
 layout (location = 2) in vec2 texCoord;
 
-out vec3 ourColor;
 out vec2 ourTexCoord;
 
-uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-    gl_Position = transform * vec4(position, 1.0f);
-    ourColor = color;
+    gl_Position = projection * view * model * vec4(position, 1.0f);
 
     // swap the y-axis since most images have the top y-axis inversed with OpenGL's.
     ourTexCoord = vec2(texCoord.x, 1.0f - texCoord.y);
--- main.cpp
+++ main.cpp
@@ -25,6 +25,8 @@
 // keyboard inputs
 #include "keyboard.h"
 
+const GLuint WIDTH = 800, HEIGHT = 600;
+
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 int main()
@@ -40,54 +42,73 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);    
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Game", nullptr, nullptr);
-
-    int screenWidth, screenHeight;
-    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
-    if (window == nullptr)
-    {
-        fprintf(stderr, "Failed to create GLFW window.");
-        glfwTerminate();
-        return EXIT_FAILURE;
-    }
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Game", nullptr, nullptr);
     glfwMakeContextCurrent(window);
     glfwSetKeyCallback(window, keyCallback);
 
     // init GLEW.
     // use modern techniques for managing OpenGL functionality.
-    glewExperimental = GL_TRUE; 
+    glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK)
     {
         fprintf(stderr, "Failed to initialize GLEW.");
         return EXIT_FAILURE;
     }
-    glViewport(0, 0, screenWidth, screenHeight);
+    glViewport(0, 0, WIDTH, HEIGHT);
 
     // compile the default shader.
-    Shader defaultShader("default.vs", "default.frag");
+    Shader ourShader("default.vs", "default.frag");
 
-    // setup vertex data and attribute pointers.
-    GLfloat vertices[] = 
-    {
-        // positions        // colors           // Texture Coords
-        0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Top Right
-        0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Bottom Right
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Bottom Left
-        -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Top Left 
+    // setup vertex data and attribute pointers
+    GLfloat vertices[] = {
+        // positions          // texture coordinates
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
-
     GLuint indices[] = {  // Note that we start from 0!
         0, 1, 3, // First Triangle
         1, 2, 3  // Second Triangle
-    };
-
-    GLfloat texCoords[] =
-    {
-        0.0f, 0.0f, // lower left
-        1.0f, 0.0f, // lower right
-        0.5f, 1.0f  // top center
     };
 
     // generate Vertex Buffer Object and Vertex Array Object and Element Buffer Object.
@@ -95,27 +116,23 @@ int main()
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
-    
+
     glBindVertexArray(VAO);
-    
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // position attribute.
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0); 
+    // position attribute (position = 0).
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0); // location = 0
 
-    // color attribute.
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1); // location = 1
-
-    // uv attribute.
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    // uv attribute (position = 2)
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(2);
-    
+
     // cleanup.
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -154,54 +171,64 @@ int main()
     // create the keyboard.
     KeyBoard keyboard;
 
-    // main loop.
+    // Game loop
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
 
         // bind textures using texture units.
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
-        glUniform1i(glGetUniformLocation(defaultShader.Program, "ourTexture1"), 0);
+        glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture1"), 0);
 
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
-        glUniform1i(glGetUniformLocation(defaultShader.Program, "ourTexture2"), 1);
+        glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture2"), 1);
 
-        // use default shader.
-        defaultShader.use();
+        // use our shader.
+        ourShader.use();
 
-        // play around with matrix transformations.
-        glm::mat4 trans;
-        trans = glm::rotate(trans, glm::radians((GLfloat)glfwGetTime() * 50.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        // create the transformation matrices.
+        glm::mat4 model, view, projection;
+        model = glm::rotate(model, ((GLfloat)glfwGetTime()) * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(glm::radians(45.0f), ((GLfloat)WIDTH) / ((GLfloat)HEIGHT), 0.1f, 100.0f);
 
-        // get/set matrix uniform location.
-        GLuint transformLoc = glGetUniformLocation(defaultShader.Program, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+        // get the uniform locations.
+        GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
+        GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
+        GLint projectionLoc = glGetUniformLocation(ourShader.Program, "projection");
+
+        // pass them to the shaders.
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection)); // this could be set only once.
 
         // draw the vertices.
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
-    
+
         // swap screen buffers.
         glfwSwapBuffers(window);
 
         if (keyboard.isPressed('q'))
         {
-            PlaySound(TEXT("./audioFiles/testViolin.wav"), NULL, SND_FILENAME | SND_ASYNC);
+            PlaySound(TEXT("./data/audioFiles/testViolin.wav"), NULL, SND_FILENAME | SND_ASYNC);
         }
     }
-
+    // Properly de-allocate all resources once they've outlived their purpose
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-
+    // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
-    return EXIT_SUCCESS;
+    return 0;
 }
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
@@ -211,4 +238,3 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
 }
-
